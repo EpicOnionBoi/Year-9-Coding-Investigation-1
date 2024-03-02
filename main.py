fec0cf1f08f4ada72d29278125bb06391b049b3b
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-03-02 07:25:42 +0000

    Made function for projection and added returns

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def compound_numbers():
     rate = float(input("Interest rate (in percent): "))
     time = input("Time unit for interest (year, quarter, month, week, day): ")
     compoundrate = input("Time unit for compounding (year, quarter, month, week, day, custom): ")
+    return principal, rate, time, compoundrate
 
 def simple_numbers():
     principal = float(input("Principal amount (in dollars): "))
     rate = float(input("Interest rate (in percent): "))
     time = input("Time unit for interest (year, quarter, month, week, day): ")
+    return principal, rate, time
+
+def projection():
+    projectionunit = input("What unit is used to project into the future (year, quarter, month, week, day): ")
+    projectiontime = float(input("Enter the amount of", projectionunit+"s", "to project into the future: "))
+    return projectionunit, projectiontime
