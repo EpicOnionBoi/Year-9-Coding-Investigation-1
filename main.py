82fc2aca898eab4b1df845b05421bee1f1fd918b
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-03-02 07:02:15 +0000

    Made functions to gather data for CI and SI

---
+++ main.py
@@ -0,0 +1,10 @@
+def compound_numbers():
+    principal = float(input("Principal amount (in dollars): "))
+    rate = float(input("Interest rate (in percent): "))
+    time = input("Time unit for interest (year, quarter, month, week, day): ")
+    compoundrate = input("Time unit for compounding (year, quarter, month, week, day, custom): ")
+
+def simple_numbers():
+    principal = float(input("Principal amount (in dollars): "))
+    rate = float(input("Interest rate (in percent): "))
+    time = input("Time unit for interest (year, quarter, month, week, day): ")
