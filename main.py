beb82a08220af41a446c97de3e71f5a1ebf3df8f
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-03-14 13:31:53 +0000

    Finished part one, made projection function for compound interest

--- main.py
+++ main.py
@@ -20,12 +20,25 @@ def simple_calculations(simpleaccount, future):
     interestrate = (simpleaccount["rate"]*time_units[simpleaccount["time"]])/100
     interest = simpleaccount["principal"]*interestrate*(future["projection time"]/time_units[future["projection unit"]])
     total = simpleaccount["principal"]+interest
-    return total
+    return round(total, 2)
+
+def compound_calculations(compoundaccount,future):
+    yearlyinterestrate = (compoundaccount["rate"]*time_units[compoundaccount["time"]])/100
+    P = compoundaccount["principal"]
+    R = yearlyinterestrate
+    N = time_units[compoundaccount["compound rate"]]
+    T = future["projection time"]/time_units[future["projection unit"]]
+    total = P*(1+(R/N))**(N*T)
+    return round(total, 2)
 
 global time_units
 time_units = {'year' : 1, 'quarter' : 4, 'month' : 12, 'week' : 52, 'day' : 365}
 print("SIMPLE INTEREST SAVINGS ACCOUNT\n")
 simpleaccount = simple_numbers()
+print("COMPOUND INTEREST SAVINGS ACCOUNT\n")
 compoundaccount = compound_numbers()
 future = projection()
-print(simple_calculations(simpleaccount, future))
+simpleinterestearned = simple_calculations(simpleaccount, future) - simpleaccount["principal"]
+compoundinterestearned = compound_calculations(compoundaccount, future) - compoundaccount["principal"]
+print("In a simple interest account, with the data you entered, you would end up with", "$" + str(simple_calculations(simpleaccount, future))+".", "The total interest earned is", "$" + simpleinterestearned)
+print("In a compound interest account, with the data you entered, you would end up with", "$" + str(compound_calculations(compoundaccount, future))".", "The total interest earned is", "$" + compoundinterestearned)
