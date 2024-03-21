e10212ab2ffcb6b29ded35eabb7b74fee283a16f
EpicOnionBoi <103018129+EpicOnionBoi@users.noreply.github.com>
2024-03-21 14:09:00 +0000

    Made the whole thing loop until it exits

--- main.py
+++ main.py
@@ -68,111 +68,112 @@ def loopington(account):
 
 global time_units # mades time units a global variable
 time_units = {"year" : 1, "quarter" : 4, "month" : 12, "week" : 52, "day" : 365} # the dictionary used to convert a lot of data into per year format
-
-print("WELCOME TO INTEREST CALCULATIONS AND STUFF\n\nTHIS PROGRAM HAS 5 MODULES\n\nMODULE 1: COMPARING SIMPLE AND COMPOUND INTEREST ACCOUNTS\nMODULE 2: HOW LONG IT WILL TAKE FOR A CI ACCOUNT TO REACH A TARGET\nMODULE 3: COMPARE 2 CI ACCOUNTS\nMODULE 4: MODEL A CI SAVINGS ACCOUNT WITH REGULAR DEPOSITS\nMODULE 5: A SIMULATION THAT COMPARES DIFFERENT COMPOUND RATES") # homepage
-module = input("\nENTER NUMBERS 1-5, OR ANYTHING ELSE TO EXIT: ") # asks for which module the user wants
-if module == "1": # if the user wants module 1
-    print("\nSIMPLE INTEREST SAVINGS ACCOUNT\n") # prints a heading
-    simpleaccount = simple_numbers() # calls the function to gather data
-    print("\nCOMPOUND INTEREST SAVINGS ACCOUNT\n") # prints a heading
-    compoundaccount = compound_numbers(module) # calls the function to gather data
-    print("\nPROJECTION INTO FUTURE\n") # prints a heading
-    future = projection() # calls the function to get projection data
-    simpleinterestearned = round(simple_calculations(simpleaccount, future) - simpleaccount["principal"], 2) # calculates the interest earned for the simple interest account
-    compoundinterestearned = round(compound_calculations(compoundaccount, future) - compoundaccount["principal"], 2) # calculates the interest earned for the compound account
-    print(f"\nIn a simple intereset account, with the data you entered, you would end up with ${simple_calculations(simpleaccount, future)}. The total interest earned is $ {simpleinterestearned}.") # prints the data for simple interest account
-    print(f"In a compound interest account, with the data you entered, you would end up with ${compound_calculations(compoundaccount, future)}. The total interest earned is ${compoundinterestearned}.") # prints the data for ccompound interest account
-elif module == "2": # if the user wants module 2
-    compoundaccount = compound_numbers(module) # calls the function to gather data
-    currentamount = compoundaccount["principal"] # defines the current amount as the principal
-    periods = 0 # defines the number of compounds so far as 0
-    if compoundaccount["compound rate"] in time_units: # if the user does not want a custom compound rate
-        interestpercompound = (compoundaccount["rate"]/(time_units[compoundaccount["compound rate"]]/time_units[compoundaccount["time"]]))/100 # calculates the interest per compound
-    else: # if the user wants  a custom compound rate
-        interestpercompound = (compoundaccount["rate"]/(compoundaccount["compound rate"]/time_units[compoundaccount["time"]]))/100 # calculates interest per compound
-    progression = [] # creates an empty list to put progressions in
-    while currentamount < compoundaccount["target amount"]: # while the current amount is less than the target amount
-        periods += 1 # adds one to the number of compounds
-        currentamount = calculations235(currentamount, interestpercompound) # calls the function to do the calculations
-        progression.append(round(currentamount, 2)) # adds the current amount to a list
-    print(f"\nHere is a list of the progression of the money until the target was reached: {progression}\n") # prints the list with the progressions in it
-    print(f"A total of {periods} compounds are needed to reach the target") # prints the number of compounds needed to reach the target
-elif module == "3":
-    print("\nCOMPOUND ACCOUNT 1:\n")
-    account1 = compound_numbers(module)
-    future1 = projection()
-    print("\nCOMPOUND ACCOUNT 2:\n")
-    account2 = compound_numbers(module)
-    future2 = projection()
-    if account1["compound rate"] in time_units:
-        compoundrate1 = time_units[account1["compound rate"]]
+loop = True
+while loop ==True:
+    print("WELCOME TO INTEREST CALCULATIONS AND STUFF\n\nTHIS PROGRAM HAS 5 MODULES\n\nMODULE 1: COMPARING SIMPLE AND COMPOUND INTEREST ACCOUNTS\nMODULE 2: HOW LONG IT WILL TAKE FOR A CI ACCOUNT TO REACH A TARGET\nMODULE 3: COMPARE 2 CI ACCOUNTS\nMODULE 4: MODEL A CI SAVINGS ACCOUNT WITH REGULAR DEPOSITS\nMODULE 5: A SIMULATION THAT COMPARES DIFFERENT COMPOUND RATES") # homepage
+    module = input("\nENTER NUMBERS 1-5, OR ANYTHING ELSE TO EXIT: ") # asks for which module the user wants
+    if module == "1": # if the user wants module 1
+        print("\nSIMPLE INTEREST SAVINGS ACCOUNT\n") # prints a heading
+        simpleaccount = simple_numbers() # calls the function to gather data
+        print("\nCOMPOUND INTEREST SAVINGS ACCOUNT\n") # prints a heading
+        compoundaccount = compound_numbers(module) # calls the function to gather data
+        print("\nPROJECTION INTO FUTURE\n") # prints a heading
+        future = projection() # calls the function to get projection data
+        simpleinterestearned = round(simple_calculations(simpleaccount, future) - simpleaccount["principal"], 2) # calculates the interest earned for the simple interest account
+        compoundinterestearned = round(compound_calculations(compoundaccount, future) - compoundaccount["principal"], 2) # calculates the interest earned for the compound account
+        print(f"\nIn a simple intereset account, with the data you entered, you would end up with ${simple_calculations(simpleaccount, future)}. The total interest earned is $ {simpleinterestearned}.") # prints the data for simple interest account
+        print(f"In a compound interest account, with the data you entered, you would end up with ${compound_calculations(compoundaccount, future)}. The total interest earned is ${compoundinterestearned}.") # prints the data for ccompound interest account
+    elif module == "2": # if the user wants module 2
+        compoundaccount = compound_numbers(module) # calls the function to gather data
+        currentamount = compoundaccount["principal"] # defines the current amount as the principal
+        periods = 0 # defines the number of compounds so far as 0
+        if compoundaccount["compound rate"] in time_units: # if the user does not want a custom compound rate
+            interestpercompound = (compoundaccount["rate"]/(time_units[compoundaccount["compound rate"]]/time_units[compoundaccount["time"]]))/100 # calculates the interest per compound
+        else: # if the user wants  a custom compound rate
+            interestpercompound = (compoundaccount["rate"]/(compoundaccount["compound rate"]/time_units[compoundaccount["time"]]))/100 # calculates interest per compound
+        progression = [] # creates an empty list to put progressions in
+        while currentamount < compoundaccount["target amount"]: # while the current amount is less than the target amount
+            periods += 1 # adds one to the number of compounds
+            currentamount = calculations235(currentamount, interestpercompound) # calls the function to do the calculations
+            progression.append(round(currentamount, 2)) # adds the current amount to a list
+        print(f"\nHere is a list of the progression of the money until the target was reached: {progression}\n") # prints the list with the progressions in it
+        print(f"A total of {periods} compounds are needed to reach the target") # prints the number of compounds needed to reach the target
+    elif module == "3":
+        print("\nCOMPOUND ACCOUNT 1:\n")
+        account1 = compound_numbers(module)
+        future1 = projection()
+        print("\nCOMPOUND ACCOUNT 2:\n")
+        account2 = compound_numbers(module)
+        future2 = projection()
+        if account1["compound rate"] in time_units:
+            compoundrate1 = time_units[account1["compound rate"]]
+        else:
+            compoundrate1 = account1["compound rate"]
+        interestpercompound1 = (account1["rate"]/(compoundrate1/time_units[account1["time"]]))/100
+        currentamount1 = account1["principal"]
+        progression1 = []
+        for e in range(int((compoundrate1)*future1["projection time"])):
+            currentamount1 = calculations235(currentamount1, interestpercompound1)
+            progression1.append(round(currentamount1, 2))
+        if account2["compound rate"] in time_units:
+            compoundrate2 = time_units[account2["compound rate"]]
+        else:
+            compoundrate2 = account2["compound rate"]
+        interestpercompound2 = (account2["rate"]/(compoundrate2/time_units[account2["time"]]))/100
+        currentamount2 = account2["principal"]
+        progression2 = []     
+        for e in range(int((compoundrate2)*future2["projection time"])):
+            currentamount2 = calculations235(currentamount2, interestpercompound2)
+            progression2.append(round(currentamount2, 2))
+        print(f"\nIn the first account, the progression throughout the projection is {progression1}\n\nThe total amount at the end is {progression1[-1]}\n\nIn the second account, the progression throughout the projection is {progression2}\n\nThe total amount at the end is {progression2[-1]}")
+    elif module == "4":
+        account = compound_numbers(module)
+        principals = []
+        interests = []
+        deposits = []
+        finals = []
+        currentamount = account["principal"]
+        if account["compound rate"] in time_units:
+            compoundrate = time_units[account["compound rate"]]
+        else:
+            compoundrate = account["compound rate"]
+        interestpercompound = (account["rate"]/(compoundrate/time_units[account["time"]]))/100
+        depositamount = account["deposit amount"]
+        if account["target amount"] == 0:
+            for e in range(int((compoundrate)*account["into future"])):
+                principals.append(round(currentamount, 2))
+                interest = currentamount * interestpercompound
+                currentamount += interest + depositamount
+                deposits.append(depositamount)
+                finals.append(round(currentamount, 2))
+                interests.append(round(interest, 2))
+        else:
+            while currentamount < account["target amount"]:
+                principals.append(round(currentamount, 2))
+                interest = currentamount * interestpercompound
+                currentamount += interest + depositamount
+                deposits.append(depositamount)
+                finals.append(round(currentamount, 2))
+                interests.append(round(interest, 2))
+        data = principals, interests, deposits, finals
+        data = list(map(list, zip(*data)))
+        header = ["Principal", "Interest", "Deposit", "Final"]
+        print("Here is a table of the progression of your money over time\n\n")
+        print(tabulate(data, tablefmt="grid", headers=header))
+    elif module == "5":
+        quarterly = {"principal":1000, "rate":100, "time":"year", "compound rate":"quarter", "projection unit":"year", "projection time":1}
+        weekly = {"principal":1000, "rate":100, "time":"year", "compound rate":"week", "projection unit":"year", "projection time":1}
+        daily = {"principal":1000, "rate":100, "time":"year", "compound rate":"day", "projection unit":"year", "projection time":1}
+        hourly = {"principal":1000, "rate":100, "time":"year", "compound rate":8760, "projection unit":"year", "projection time":1}
+        tenminutely = {"principal":1000, "rate":100, "time":"year", "compound rate":52704, "projection unit":"year", "projection time":1}
+        quarterlyamount = round(loopington(quarterly), 2)
+        weeklyamount = round(loopington(weekly), 2)
+        dailyamount = round(loopington(daily), 2)
+        hourlyamount = round(loopington(hourly), 2)
+        tenminutelyamount = round(loopington(tenminutely), 2)
+        print(f"If you compound quarterly, you would end up with ${quarterlyamount}\nIf you compound weekly, you would end up with${weeklyamount}\nIf you compound daily, you would end up with ${dailyamount}\nIf you compound hourly, you would end up with ${hourlyamount}\nIf you compound every ten minutes, you would end up with ${tenminutelyamount}")
     else:
-        compoundrate1 = account1["compound rate"]
-    interestpercompound1 = (account1["rate"]/(compoundrate1/time_units[account1["time"]]))/100
-    currentamount1 = account1["principal"]
-    progression1 = []
-    for e in range(int((compoundrate1)*future1["projection time"])):
-        currentamount1 = calculations235(currentamount1, interestpercompound1)
-        progression1.append(round(currentamount1, 2))
-    if account2["compound rate"] in time_units:
-        compoundrate2 = time_units[account2["compound rate"]]
-    else:
-        compoundrate2 = account2["compound rate"]
-    interestpercompound2 = (account2["rate"]/(compoundrate2/time_units[account2["time"]]))/100
-    currentamount2 = account2["principal"]
-    progression2 = []     
-    for e in range(int((compoundrate2)*future2["projection time"])):
-        currentamount2 = calculations235(currentamount2, interestpercompound2)
-        progression2.append(round(currentamount2, 2))
-    print(f"\nIn the first account, the progression throughout the projection is {progression1}\n\nThe total amount at the end is {progression1[-1]}\n\nIn the second account, the progression throughout the projection is {progression2}\n\nThe total amount at the end is {progression2[-1]}")
-elif module == "4":
-    account = compound_numbers(module)
-    principals = []
-    interests = []
-    deposits = []
-    finals = []
-    currentamount = account["principal"]
-    if account["compound rate"] in time_units:
-        compoundrate = time_units[account["compound rate"]]
-    else:
-        compoundrate = account["compound rate"]
-    interestpercompound = (account["rate"]/(compoundrate/time_units[account["time"]]))/100
-    depositamount = account["deposit amount"]
-    if account["target amount"] == 0:
-        for e in range(int((compoundrate)*account["into future"])):
-            principals.append(round(currentamount, 2))
-            interest = currentamount * interestpercompound
-            currentamount += interest + depositamount
-            deposits.append(depositamount)
-            finals.append(round(currentamount, 2))
-            interests.append(round(interest, 2))
-    else:
-        while currentamount < account["target amount"]:
-            principals.append(round(currentamount, 2))
-            interest = currentamount * interestpercompound
-            currentamount += interest + depositamount
-            deposits.append(depositamount)
-            finals.append(round(currentamount, 2))
-            interests.append(round(interest, 2))
-    data = principals, interests, deposits, finals
-    data = list(map(list, zip(*data)))
-    header = ["Principal", "Interest", "Deposit", "Final"]
-    print("Here is a table of the progression of your money over time\n\n")
-    print(tabulate(data, tablefmt="grid", headers=header))
-elif module == "5":
-    quarterly = {"principal":1000, "rate":100, "time":"year", "compound rate":"quarter", "projection unit":"year", "projection time":1}
-    weekly = {"principal":1000, "rate":100, "time":"year", "compound rate":"week", "projection unit":"year", "projection time":1}
-    daily = {"principal":1000, "rate":100, "time":"year", "compound rate":"day", "projection unit":"year", "projection time":1}
-    hourly = {"principal":1000, "rate":100, "time":"year", "compound rate":8760, "projection unit":"year", "projection time":1}
-    tenminutely = {"principal":1000, "rate":100, "time":"year", "compound rate":52704, "projection unit":"year", "projection time":1}
-    quarterlyamount = round(loopington(quarterly), 2)
-    weeklyamount = round(loopington(weekly), 2)
-    dailyamount = round(loopington(daily), 2)
-    hourlyamount = round(loopington(hourly), 2)
-    tenminutelyamount = round(loopington(tenminutely), 2)
-    print(f"If you compound quarterly, you would end up with ${quarterlyamount}\nIf you compound weekly, you would end up with${weeklyamount}\nIf you compound daily, you would end up with ${dailyamount}\nIf you compound hourly, you would end up with ${hourlyamount}\nIf you compound every ten minutes, you would end up with ${tenminutelyamount}")
-else:
-    print("""
+        print("""
                                     µçççççççµ
                             µ▄æ╧╜╩ññ.  ▄▄▄æ##æ▄▓▀▄,
                         ▄#╜²         ¿ççç▄▄▄▄▄▄▄▄▄,;▀gµ
@@ -220,3 +221,4 @@ else:
                             ²▄▄▓▓▀▀█▓▒▓▀▀█▄ç Ñ
                                     ╙*    ╛
         """)
+        loop = False
